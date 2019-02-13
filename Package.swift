487e996a4bf458d2bee80ea54142ca243069ebaa
Rajesh Panda <rajesh_panda@external.mckinsey.com>
2019-02-13 19:10:12 +0530

    initial draft

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "WaffleSpot",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
+
